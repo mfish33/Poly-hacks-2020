b9b08cc63c86835da05375481c727aa7ae106ddc
Kyle-McRae <kyle.mcrae1@gmail.com>
2020-02-08 16:42:55 -0800

    Made Utils class and getInput()

--- scripts/LoginService.ts
+++ scripts/LoginService.ts
@@ -6,39 +6,33 @@ export default class LoginService{
     constructor(private app: any){}
 
      public async login() {
-        let email = (<HTMLInputElement>document.getElementById('loginPage-email')).value
-        let password = (<HTMLInputElement>document.getElementById('loginPage-password')).value
-        if(this.emailRegex.test(email)){
-            try{
-                if(!email || !password){
-                    throw ""
-                }
-                this.app.messageService = await MessageService.signIn(email, password)
-            }catch(e){
-                // TODO: Show error
-                throw new Error(`Could not sign in ${e}`)
+        try{
+            let[email, password] = Utils.getInput('loginPage-email', 'loginPage-password')
+                if(this.emailRegex.test(email)){
+                    if(!email || !password){
+                        throw ""
+                    }
+                    this.app.messageService = await MessageService.signIn(email, password)
             }
-        }else{
-
+        }catch(e){
+            // TODO: Show error
+            throw new Error(`Could not sign in ${e}`)
         }
     }
     public async register() {
-        let email = (<HTMLInputElement>document.getElementById('registerPage-email')).value
-        let password = (<HTMLInputElement>document.getElementById('registerPage-password')).value
-        let displayName = (<HTMLInputElement>document.getElementById('registerPage-displayName')).value
-         let avatarLocation = (<HTMLInputElement>document.getElementById('registerPage-avatar')).value
-         let avatar = avatarLocation ? avatarLocation : "null"
-        if(this.emailRegex.test(email)){
-            try{
-                if(!email || !password || !displayName){
-                    throw ""
+        try{
+            let [email,displayName,password] = Utils.getInput('registerPage-email', 'registerPage-displayName', 'registerPage-password')
+                if(this.emailRegex.test(email)){
+                    if(!email || !password || !displayName){
+                        throw ""
+                    }
+                    this.app.messageService = await MessageService.register(email, password, displayName)
                 }
-                 this.app.messageService = await MessageService.register(email, password, displayName)
-            }catch(e){
-                // TODO: Show error
-                throw new Error(`Could not register ${e}`)
-            }
+        }catch(e){
+            // TODO: Show error
+            throw new Error(`Could not register ${e}`)
         }
+        
     }
 
 }
---
+++ scripts/Utils.ts
@@ -0,0 +1,9 @@
+class Utils{
+    static getInput(...args:string[]) {
+        try{
+            return args.map(prop => (<HTMLInputElement>document.getElementById(prop)).value)
+        } catch(e) {
+            throw new Error('Invalid id names')
+        }
+    }
+}
