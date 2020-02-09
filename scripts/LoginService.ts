75495f76a2e089e6efbeed9ae031965b9aeb26e5
Kyle-McRae <kyle.mcrae1@gmail.com>
2020-02-08 16:08:28 -0800

    worked on login and register

--- scripts/LoginService.ts
+++ scripts/LoginService.ts
@@ -1,18 +1,44 @@
+import MessageService from "./firebase/MessageService"
+   
 export default class LoginService{
+    private emailRegex = RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
 
     constructor(private app: any){}
 
-    public async login() {
+     public async login() {
         let email = (<HTMLInputElement>document.getElementById('loginPage-email')).value
         let password = (<HTMLInputElement>document.getElementById('loginPage-password')).value
-        
+        if(this.emailRegex.test(email)){
+            try{
+                if(!email || !password){
+                    throw ""
+                }
+                this.app.messageService = await MessageService.signIn(email, password)
+            }catch(e){
+                // TODO: Show error
+                throw new Error(`Could not sign in ${e}`)
+            }
+        }else{
 
+        }
     }
     public async register() {
         let email = (<HTMLInputElement>document.getElementById('registerPage-email')).value
         let password = (<HTMLInputElement>document.getElementById('registerPage-password')).value
         let displayName = (<HTMLInputElement>document.getElementById('registerPage-displayName')).value
-        let avatar = (<HTMLInputElement>document.getElementById('registerPage-avatar')).value
+         let avatarLocation = (<HTMLInputElement>document.getElementById('registerPage-avatar')).value
+         let avatar = avatarLocation ? avatarLocation : "null"
+        if(this.emailRegex.test(email)){
+            try{
+                if(!email || !password || !displayName){
+                    throw ""
+                }
+                 this.app.messageService = await MessageService.register(email, password, displayName)
+            }catch(e){
+                // TODO: Show error
+                throw new Error(`Could not register ${e}`)
+            }
+        }
     }
 
 }
